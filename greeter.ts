754e6faeaa0ef92efdfa8b8420fe9eb5723e933b
Hajrudin Coralic <hajrudinco@hotmail.com>
2019-01-25 13:45:32 +0100

    simple greeter.ts

---
+++ greeter.ts
@@ -0,0 +1,33 @@
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+/**
+ * Best class ever.
+ */
+class Developer {
+    /** This is some property description */
+    fullName: string;
+
+    /**
+     * Testing the docstrings.
+     * @param firstName This is first name
+     * @param lastName This is last name
+     */
+    constructor(public firstName: string, public lastName: string) {
+        this.fullName = `${firstName} ${lastName}`;
+    }
+}
+
+/**
+ * console.log - greets the person.
+ * @param person 
+ */
+function greeter(person: Person) {
+    console.log(`Hello, ${person.firstName} ${person.lastName}`);
+}
+
+const user = new Developer('Hajrudin', 'Coralic');
+
+greeter(user);
